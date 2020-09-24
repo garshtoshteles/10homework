3a6f7e3929e86bae97917a1a2d5441ee3a3abefa
garshtoshteles <tj.yuxn.scott@gmail.com>
2020-09-24 13:55:02 -0400

    added post route, everything now works.

--- server.js
+++ server.js
@@ -3,45 +3,54 @@ var express = require("express"),
   fs = require("fs"),
   app = express(),
   PORT = process.env.PORT || 3000;
-app.use(express.urlencoded({ extended: !0 })),
+app.use(express.urlencoded({ extended: true })),
   app.use(express.json()),
   app.use(express.static("public"));
 let storedNotes = [];
-//
-// ROUTES
-//
-app.get("/", function (a, b) {
-  b.sendFile(path.join(__dirname, "./public/index.html"));
+app.get("/", function (c, a) {
+  a.sendFile(path.join(__dirname, "./public/index.html"));
 }),
-  app.get("/notes", function (a, b) {
-    b.sendFile(path.join(__dirname, "./public/notes.html"));
+  app.get("/notes", function (c, a) {
+    a.sendFile(path.join(__dirname, "./public/notes.html"));
   }),
-  app.get("/api/notes", function (a, b) {
+  app.get("/api/notes", function (c, a) {
     try {
       (storedNotes = fs.readFileSync("./db/db.json", "utf8")),
         (storedNotes = JSON.parse(storedNotes));
-    } catch (a) {
+    } catch (b) {
       console.log("Error: API-stored notes not retrieved");
     }
-    b.json(storedNotes);
+    a.json(storedNotes);
   }),
-  //
-  // ROUTE FOR POST GOES HERE
-  //
-  app.delete("/api/notes/:id", function (a, b) {
+  app.post("/api/notes", function (c, a) {
+    try {
+      (storedNotes = fs.readFileSync("./db/db.json", "utf8")),
+        (storedNotes = JSON.parse(storedNotes)),
+        (c.body.id = Math.floor(Math.random() * Math.floor(1000000))),
+        storedNotes.push(c.body),
+        (storedNotes = JSON.stringify(storedNotes)),
+        fs.writeFile("./db/db.json", storedNotes, "utf8", function (b) {
+          if (b) throw b;
+        }),
+        a.json(JSON.parse(storedNotes));
+    } catch (b) {
+      throw b;
+    }
+  }),
+  app.delete("/api/notes/:id", function (c, a) {
     try {
       (storedNotes = fs.readFileSync("./db/db.json", "utf8")),
         (storedNotes = JSON.parse(storedNotes));
-      let c = storedNotes.filter(function (b) {
-        return b.id != a.params.id;
+      let b = storedNotes.filter(function (a) {
+        return a.id != c.params.id;
       });
-      (storedNotes = JSON.stringify(c)),
-        fs.writeFile("./db/db.json", storedNotes, "utf8", function (a) {
-          if (a) throw a;
+      (storedNotes = JSON.stringify(b)),
+        fs.writeFile("./db/db.json", storedNotes, "utf8", function (b) {
+          if (b) throw b;
         }),
-        b.send(JSON.parse(storedNotes));
-    } catch (a) {
-      throw a;
+        a.send(JSON.parse(storedNotes));
+    } catch (b) {
+      throw b;
     }
   }),
   app.listen(PORT, function () {
